6b9308216f00ef75b87d7c6e050e82bc1bec616a
user-master <ocean.light.0611@icloud.com>
2026-09-20 18:32:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 保存済みの内容を優先して起動する（公開ページが無くなっても、iPhoneに保存された分で動き続ける）
-const VER = "derm-ichimon-lock-2";
+const VER = "derm-ichimon-lock-7ffcae03";
 const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "maskable-512.png", "apple-touch-icon.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(VER).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== VER).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
